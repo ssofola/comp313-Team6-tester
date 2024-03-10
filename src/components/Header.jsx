6e65c8ae3fc2d3c1d198a6be4a447363b290a21c
Olusegun Sofola <ssofola@gmail.com>
2024-03-10 19:18:48 -0400

    "third commit"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-import logoImg from '../assets/quiz-logo.png';
+import logoImg from '../assets/logo.png';
 
 export default function Header() {
   return (
     <header>
       <img src={logoImg} alt="Quiz logo" />
-      <h1>ReactQuiz</h1>
+      <h1>MindQuiz</h1>
     </header>
   );
 }
